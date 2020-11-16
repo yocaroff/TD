d3eee62c73255be6253a18ecf4332cf971a86836
yocaroff <yohan.caroff@hotmail.fr>
2020-11-16 11:49:02 +0100

    first commit

---
+++ app/App.js
@@ -0,0 +1,29 @@
+class App {
+
+    static start() {
+        //onpopstate
+        window.onpopstate = function() {
+            App.browse();
+        };
+
+        //correction burger
+        $('.nav-link').on('click', function(evt) {
+            if ($(window).width() < 992) {
+                let btn = $(evt.target).closest('.navbar').find('.navbar-toggler').not('collapsed');
+                btn ? btn.click() : null;
+            }
+        }) 
+
+        //chargement de la page
+        $(document).ready(function() {
+            App.browse()
+        }); 
+    }
+
+    static browse() {
+        //récupérer le hash et l'afficher dans main
+        let hash = (window.location.hash || "#accueil").substring(1);
+        $('main').hide().html(hash).fadeIn(100)
+    }
+
+}
